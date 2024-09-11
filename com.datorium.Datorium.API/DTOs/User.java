ce3132a94e3c6638210b6876a4819668c123f643
lasmameziele <165931777+lasmameziele@users.noreply.github.com>
2024-09-11 20:13:59 +0300

    Create User.java

---
+++ com.datorium.Datorium.API/DTOs/User.java
@@ -0,0 +1,23 @@
+package com.datorium.Datorium.API.DTOs;
+
+public class User {
+    private int id;
+    private String name;
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
